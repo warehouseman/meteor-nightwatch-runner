e5a6f5533f57176b82532ac4c161b046f68c1613
mhb.warehouseman@gmail.com <Martin H. J. Bramwell>
2015-08-10 19:08:15 -0400

    Working dependencies  Changes committed: 	modified:   meteor-nightwatch-runner.run 	modified:   tests/nightwatch/bin/install-nightwatch-dependencies.sh

--- tests/nightwatch/bin/install-nightwatch-dependencies.sh
+++ tests/nightwatch/bin/install-nightwatch-dependencies.sh
@@ -6,11 +6,14 @@
 # echo "  -- Ready to install NightWatch runner dependencies in -- $(pwd)."
 echo "  -- Ready to install NightWatch runner dependencies."
 #
+export GLOBAL_NODEJS=$(npm config get prefix)
+export GLOBAL_NODEJS_MODULES=${GLOBAL_NODEJS}/lib/node_modules
+#
 export LOCAL_NODEJS=${HOME}
 export LOCAL_NODEJS_MODULES=${LOCAL_NODEJS}/node_modules
 mkdir -p ${LOCAL_NODEJS_MODULES}
 #
-modules=( "touch" "bunyan" "underscore" "mkdirp" "nightwatch" "chromedriver" "minimist" "fs-extra" "request" )
+modules=( "touch" "bunyan" "underscore" "mkdirp" "nightwatch" "npm" "chromedriver" "minimist" "fs-extra" "request" )
 for idx in "${modules[@]}"
 do
   :
@@ -22,14 +25,20 @@ do
   fi;
 done
 #
+MDL="bunyan"
+if [ ! -d ${GLOBAL_NODEJS_MODULES}/${MDL}/ ]; then
+  echo "Installing '${MDL}' in directory -- ${GLOBAL_NODEJS_MODULES}."
+  npm install -y --global --prefix ${GLOBAL_NODEJS} ${MDL};
+else
+  echo "Node module '${MDL}' is already available.";
+fi;
+
 echo "  -- linking to chronedriver -- ${LOCAL_NODEJS_MODULES}."
-ln -s ${LOCAL_NODEJS_MODULES}/chromedriver chromedriver
+ln -s ${LOCAL_NODEJS_MODULES}/chromedriver $(dirname $0)/chromedriver
 #
-echo "  -- installing Selenium in directory -- ${LOCAL_NODEJS_MODULES} $(pwd)."
+echo "  -- installing Selenium in directory -- ${LOCAL_NODEJS_MODULES} $(dirname $0)."
+
 wget -P ${LOCAL_NODEJS_MODULES} --no-clobber http://selenium-release.storage.googleapis.com/2.47/selenium-server-standalone-2.47.1.jar
-ln -s ${LOCAL_NODEJS_MODULES}/selenium-server-standalone-2.47.1.jar selenium-server-standalone.jar
+ln -s ${LOCAL_NODEJS_MODULES}/selenium-server-standalone-2.47.1.jar $(dirname $0)/selenium-server-standalone.jar
 #
 echo "Dependencies loaded."
-
-
-
